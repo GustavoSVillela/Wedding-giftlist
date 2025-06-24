2a62c0e8fa2aeac2c8b9cd3682af739790d00136
GustavoSVillela <villelagustavo33@gmail.com>
2025-06-24 16:30:05 -0300

    update countdown

--- js/scriptregressivo.js
+++ js/scriptregressivo.js
@@ -3,30 +3,53 @@ const hoursElements = document.querySelector("[data-hours]")
 const minutesElements = document.querySelector("[data-minutes]")
 const secondsElements = document.querySelector("[data-seconds]")
 
-const render =(days, hours, minutes, seconds)=>{
+function FormatNumber(n){
+    return n.toString().padStart(2,"0")
+}
+
+const render =(days, hours, minutes, seconds)=>{ //function to show countdown 
     daysElements.innerHTML = days
     hoursElements.innerHTML = hours
     minutesElements.innerHTML = minutes
     secondsElements.innerHTML = seconds
 };
 
-const coutdown=()=>{
-const now = new Date()
-const nextYear = now.getFullYear()
-const targetDate = new Date(nextYear, 6, 26)
-
-const timeleft = targetDate - now
-
-const days = Math.floor(timeleft / (1000 *60 *60 *24))
-const hours = Math.floor(
-    (timeleft % (1000 *60 *60 *24))/(1000*60 *60)
-)
-
-const minutes = Math.floor((timeleft % (1000 *60 *60))/(1000*60))
-const seconds = Math.floor((timeleft % (1000 *60))/1000)
+const targetDate = new Date(2025,6,26,20,0,0)
 
 
-render(days,hours,minutes,seconds)
+const messageElement=document.querySelector("#message")
+
+const countdown=()=>{ // arrow function to countdown
+const now = new Date() // take atual day
+const timeleft = targetDate - now //calc Now - target day
+
+if(timeleft <= 86400000 && !mensagemMostrada){
+    messageElement.innerText="⏳ Faltam menos de 24 horas para a festa! Prepare-se!"
+} else if (timeleft <= 86400000 && !avisouUltimoDiaMostrado){
+    
 }
 
-setInterval(coutdown,1000)
+
+if(timeleft<=0){
+    clearInterval(intervalId) //Stop the count 
+    render("00","00","00","00")
+
+    messageElement.innerText = "🎉 Hoje é o dia da nossa festa de despedida! 🎉"
+    messageElement.style.display = "block"
+    return
+}
+
+const days = Math.floor(timeleft / (1000 *60 *60 *24)) // calc days
+const hours = Math.floor((timeleft % (1000 *60 *60 *24))/(1000*60 *60)) // calc hours
+const minutes = Math.floor((timeleft % (1000 *60 *60))/(1000*60)) //calc minutes
+const seconds = Math.floor((timeleft % (1000 *60))/1000) //calc seconds
+
+render(
+    FormatNumber(days),
+    FormatNumber(hours),
+    FormatNumber(minutes),
+    FormatNumber(seconds)
+   )
+}
+
+const intervalId = setInterval(countdown,1000)

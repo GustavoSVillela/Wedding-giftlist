0c1fd35c4fe8e972f6b7d980b8f45ec383cf8c98
GustavoSVillela <villelagustavo33@gmail.com>
2025-06-24 08:01:00 -0300

    update index.html

---
+++ js/scriptregressivo.js
@@ -0,0 +1,4 @@
+const daysElements = document.querySelector("[data-days]")
+const hoursElements = document.querySelector("[data-hours]")
+const minutesElements = document.querySelector("[data-minutes]")
+const secondsElements = document.querySelector("[data-seconds]")
